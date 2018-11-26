35d0e46a865112749f2cd05a4936ebb0d8f90fff
guochao_he <Guochao.He@cienet.com>
2018-11-25 22:00:32 -0500

    create actioncreaor and constants

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { connect } from 'react-redux';
+import { actionCreators } from './store';
 import {
   HeaderWrapper,
   Logo,
@@ -62,16 +63,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return{
     handleInputFocus(){
-      const action = {
-        type: 'search_focus'
-      };
-      dispatch(action)
+      dispatch(actionCreators.searchFocus())
     },
     handleInputBlur(){
-      const action = {
-        type: 'search_blur'
-      };
-      dispatch(action)
+      dispatch(actionCreators.searchBlur())
     },
   }
 };
