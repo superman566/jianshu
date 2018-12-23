58dec067376365f8f7545216501f8af2a2e3f03a
guochao_he <Guochao.He@cienet.com>
2018-12-23 13:02:17 -0500

    optimate ajax request

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -58,7 +58,7 @@ class Header extends Component {
   }
 
   render() {
-    const { focused, handleInputFocus, handleInputBlur } = this.props;
+    const { focused, handleInputFocus, handleInputBlur, list } = this.props;
     return(
       <HeaderWrapper>
         <Logo />
@@ -82,7 +82,7 @@ class Header extends Component {
             >
               <NavSearch
                 className={focused ? 'focused': ''}
-                onFocus = {handleInputFocus}
+                onFocus = {()=>{handleInputFocus(list)}}
                 onBlur = {handleInputBlur}
               />
             </CSSTransition>
@@ -113,8 +113,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return{
-    handleInputFocus(){
-      dispatch(actionCreators.getList());
+    handleInputFocus(list){
+      (list.size === 0) && dispatch(actionCreators.getList());
       dispatch(actionCreators.searchFocus())
     },
     handleInputBlur(){
