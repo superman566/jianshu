31f0da35ac9a320e23eecbd252a9471d7574bedc
guochao_he <Guochao.He@cienet.com>
2018-12-29 01:14:34 -0500

    feat: detail redux done

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  DetailWrapper,
+  Header,
+  Content,
+} from './style'
 
 class Detail extends Component {
   render() {
+    const {
+      title,
+      content,
+    } = this.props;
     return (
-      <div>
-        Detail
-      </div>
+      <DetailWrapper>
+        <Header>
+          {title}
+        </Header>
+        <Content dangerouslySetInnerHTML={{__html: content}} />
+      </DetailWrapper>
     )
   }
 }
 
-export default Detail;
+const mapState = (state) => ({
+ title: state.getIn(['detail', 'title']),
+ content: state.getIn(['detail', 'content']),
+});
+
+export default connect(mapState, null)(Detail);
