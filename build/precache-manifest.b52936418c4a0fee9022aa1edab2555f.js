77deaa570022bb2a2d22b902a82df2b8d0008fb7
guochao_he <Guochao.He@cienet.com>
2018-12-29 23:03:32 -0500

    feat: add build

---
+++ build/precache-manifest.b52936418c4a0fee9022aa1edab2555f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e11ce1f1358428b04720cf94b1c1a3e6",
+    "url": "/static/media/iconfont.e11ce1f1.svg"
+  },
+  {
+    "revision": "c7b3c4d10911d8e410a80ec0d31967d0",
+    "url": "/static/media/iconfont.c7b3c4d1.eot"
+  },
+  {
+    "revision": "659215fe69134643db566acc055ff120",
+    "url": "/static/media/iconfont.659215fe.ttf"
+  },
+  {
+    "revision": "26ecaa714d8ed263d847",
+    "url": "/static/js/runtime~main.26ecaa71.js"
+  },
+  {
+    "revision": "75a21a8148f9eb8dafa5",
+    "url": "/static/js/main.75a21a81.chunk.js"
+  },
+  {
+    "revision": "b568dced013a48248ed7",
+    "url": "/static/js/3.b568dced.chunk.js"
+  },
+  {
+    "revision": "223107680b6e34f71730",
+    "url": "/static/js/2.22310768.chunk.js"
+  },
+  {
+    "revision": "26813ebdda6b2fea410a",
+    "url": "/static/js/1.26813ebd.chunk.js"
+  },
+  {
+    "revision": "75a21a8148f9eb8dafa5",
+    "url": "/static/css/main.8cc1c8a7.chunk.css"
+  },
+  {
+    "revision": "880d8d5279591c1bbd3e07a3f63aeeca",
+    "url": "/index.html"
+  }
+];
